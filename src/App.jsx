71b2d2ed08af0124733e553fad55d5df5ed5b82a
Jeevan Shrestha <jeevan.shrestha@au.harveynorman.com>
2025-03-17 10:57:15 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import TinyMCE from "./components/tinymce/TinyMCE"
+
+
+function App() {
+  return (
+    <>
+         <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Hello, Tailwind CSS!</h1>
+        <p className="text-gray-700">This is a simple example of using Tailwind CSS in a React project.</p>
+        <TinyMCE />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
